1a07c3d2e9359135b379d2cbde58450ce8be9fbd
shaquille19 <shakil.m.19@gmail.com>
2021-05-11 18:59:41 +0600

    intialied tiny slider for home banner

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function($){
+    tns({
+        container: '.home_slider',
+        items: 1,
+        slidBy: 1,
+        controls: true,
+        controlsPosition: 'top',
+        autoplay: false,
+        nav: false
+    });
+});
