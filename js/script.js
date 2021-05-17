5f2441aebabee35ce97fdf1ff8441f9ad3d698df
shaquille19 <shakil.m.19@gmail.com>
2021-05-17 23:18:24 +0600

    initialized tiny slider for about slider

--- js/script.js
+++ js/script.js
@@ -10,4 +10,15 @@ $(function($){
         prevButton: '.slider_left_arrow',
         nextButton: '.slider_right_arrow'
     });
+
+    tns({
+        container: '.about_slider',
+        items: 1,
+        slidBy: 1,
+        controls: false,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        nav: true,
+        navContainer: '.slider_dots'
+    });
 });
