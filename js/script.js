23e391aa79ccd8bde5908c2685d49b9578971ee7
shaquille19 <shakil.m.19@gmail.com>
2021-06-08 14:50:11 +0600

    First Commit

---
+++ js/script.js
@@ -0,0 +1,175 @@
+$(function ($) {
+
+    //banner slider initialization
+
+    $('.home_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.slider_left_arrow'),
+        nextArrow: $('.slider_right_arrow')
+    });
+
+
+    //about us section slider initialization
+
+    $('.about_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        appendDots: $('.slider_sots')
+    });
+
+    $('.slider_dots').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.about_slider',
+        focusOnSelect: true
+    })
+
+    //Our courses section slider initialization
+
+    $('.course_slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.course_slider_left_arrow'),
+        nextArrow: $('.course_slider_right_arrow'),
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+    // testimonial slider initialization
+
+    $('.testimonial_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: $('.test_slide_icon_prev'),
+        nextArrow: $('.test_slide_icon_next')
+    });
+
+
+    // Our Teachers slider initialization
+
+    $('.teachers').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+    // Contact image slider initialization
+
+    $('.contact_slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 481,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+
+    // counter initialization
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+
+    // Event Calendar Initialization
+
+    var current_yyyymm_ = moment().format("YYYYMM");
+
+    $("#pb-calendar").pb_calendar({
+        'next_month_button': '<img src="./images/arrow-next.png" class="icon">',
+        'prev_month_button': '<img src="./images/arrow-prev.png" class="icon">',
+        schedule_list: function (callback_, yyyymm_) {
+            var temp_schedule_list_ = {};
+
+            temp_schedule_list_[current_yyyymm_ + "05"] = [
+                {
+                    'ID': 1,
+                    style: "red"
+                }
+              ];
+
+            temp_schedule_list_[current_yyyymm_ + "15"] = [
+                {
+                    'ID': 2,
+                    style: "red"
+                }
+            ];
+
+            temp_schedule_list_[current_yyyymm_ + "20"] = [
+                {
+                    'ID': 3,
+                    style: "green"
+                },
+            ];
+
+            temp_schedule_list_[current_yyyymm_ + "22"] = [
+                {
+                    'ID': 4,
+                    style: "green"
+                },
+              ];
+            callback_(temp_schedule_list_);
+        },
+        schedule_dot_item_render: function (dot_item_el_, schedule_data_) {
+            dot_item_el_.addClass(schedule_data_['style'], true);
+            return dot_item_el_;
+        }
+    });
+
+
+});
